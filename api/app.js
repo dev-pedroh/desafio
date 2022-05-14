109d81b79178e03b82719729c75c31597cfaefe9
dev-pedroh <phmoreira22@gmail.com>
2022-05-14 16:31:52 -0300

    adc ssl e finalizando primeira versao da api

--- api/app.js
+++ api/app.js
@@ -1,10 +1,19 @@
 import express from 'express';
+import fs from 'fs';
+import cors from 'cors';
+import https from 'https';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 import roteador from './routes/routes.js';
 
 app.use(roteador);
 
 app.listen(3000, () => console.log('Api rodando!') );
+
+https.createServer({
+    cert: fs.readFileSync('SSL/code.crt'),
+    key: fs.readFileSync('SSL/code.key')
+}, app).listen(3001, ()=> console.log("https ok"));
